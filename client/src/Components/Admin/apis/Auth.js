ff72f605766fa772043f5e4a0bf401653c0c9c6a
Bucephalus <bhargabnath691@gmail.com>
2021-09-25 21:03:01 +0530

    slight change

--- client/src/Components/Admin/apis/Auth.js
+++ client/src/Components/Admin/apis/Auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV !== 'production'
+const baseURL = process.env.NODE_ENV === 'production'
     ? 'api/auth'
     : 'http://localhost:5000/api/auth';
 
