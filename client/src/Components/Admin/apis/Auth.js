da29399313f023c99844178af69676c19a74f3f2
Bucephalus <bhargabnath691@gmail.com>
2021-09-27 12:33:32 +0530

    db

--- client/src/Components/Admin/apis/Auth.js
+++ client/src/Components/Admin/apis/Auth.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-    ? '/api/auth'
+    ? 'api/auth'
     : 'http://localhost:5000/api/auth';
+
+console.log(baseURL);
 
 export default axios.create({
     baseURL
