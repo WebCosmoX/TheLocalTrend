4765b7c4da980f9ddb0fb70d81c9a2e1ff40f8cf
Bucephalus <bhargabnath691@gmail.com>
2021-09-28 12:10:29 +0530

    .github added

--- client/src/Components/Admin/apis/Auth.js
+++ client/src/Components/Admin/apis/Auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-    ? 'http://www.thelocaltrendent.com/api/auth'
+    ? 'api/auth'
     : 'http://localhost:5000/api/auth';
 
 console.log({ baseURL });
