84c000967ecd01bd24d5c9356e34e0d848893099
Bucephalus <bhargabnath691@gmail.com>
2021-09-29 20:19:09 +0530

    client api changed

--- client/src/Components/Admin/apis/Artist.js
+++ client/src/Components/Admin/apis/Artist.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-    ? 'api/artists/'
+    ? 'https://www.thelocaltrendent.com/api/artists/'
     : 'http://localhost:5000/api/artists/';
 
 console.log({ artistUrl: baseURL });
--- client/src/Components/Admin/apis/Service.js
+++ client/src/Components/Admin/apis/Service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-    ? 'api/services/'
+    ? 'https://www.thelocaltrendent.com/api/services/'
     : 'http://localhost:5000/api/services/';
 
 console.log({ serviceUrl: baseURL });
