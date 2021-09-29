f9f6628e09d70965f4719ab31d9e635efcace6c2
Bucephalus <bhargabnath691@gmail.com>
2021-09-30 02:26:43 +0530

    deploy.sh updated

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 sudo git pull origin backend-dev
+echo 'Installing server dependencies...'
+echo '*************************'
+sudo npm install
 echo 'Entering client folder...'
 echo '*************************'
 echo '*************************'
-cd .. && client
+cd .. 
+cd client
 echo 'Installing client dependencies...'
 echo '*************************'
 echo '*************************'
 yarn
-sudo npm run build
-echo 'Entering server folder...'
-echo '*************************'
-echo '*************************'
-cd .. && cd server
-echo 'Installing server dependencies...'
-echo '*************************'
-echo '*************************'
-npm install
+npm run build
 sudo systemctl restart nginx
 sudo pm2 restart all
